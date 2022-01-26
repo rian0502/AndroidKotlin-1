6ac7cf8757089c36b6798484a7beff263619d5ad
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-01-26 16:32:07 +0700

    new activity we will go to new page

---
+++ app/src/main/java/com/belajar/kotlinandroid_1/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.belajar.kotlinandroid_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
