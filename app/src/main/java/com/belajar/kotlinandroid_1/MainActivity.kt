40b4b1baa22eecf7c4164f0be11b7518a5bf341c
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-01-28 19:13:23 +0700

    Update MainActivity.kt

--- app/src/main/java/com/belajar/kotlinandroid_1/MainActivity.kt
+++ app/src/main/java/com/belajar/kotlinandroid_1/MainActivity.kt
@@ -1,20 +1,15 @@
 package com.belajar.kotlinandroid_1
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toPageTwo = findViewById<Button>(R.id.toPageTwo);
+        val toPageTwo = findViewById<Button>(R.id.toPageTwo)
         toPageTwo.setOnClickListener{
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
