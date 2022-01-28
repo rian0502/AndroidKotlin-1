9d402257ffa638739962f8ea29d1a00d5456fa12
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-01-28 19:42:27 +0700

    Update SecondActivity.kt

--- app/src/main/java/com/belajar/kotlinandroid_1/SecondActivity.kt
+++ app/src/main/java/com/belajar/kotlinandroid_1/SecondActivity.kt
@@ -1,18 +1,15 @@
 package com.belajar.kotlinandroid_1
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
-import android.view.ViewParent
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import java.text.FieldPosition
 
 class SecondActivity : AppCompatActivity() {
-    val names = arrayOf("android", "php", "c++", "java")
+    private val names = arrayOf("android", "php", "c++", "java")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
@@ -23,15 +20,8 @@ class SecondActivity : AppCompatActivity() {
         val listView = findViewById<ListView>(R.id.listView)
         val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,names)
         listView.adapter = adapter
-        listView.onItemClickListener = object : AdapterView.OnItemClickListener{
-            override fun onItemClick(
-                parent: AdapterView<*>?,
-                view: View?,
-                position: Int,
-                id: Long
-            ){
-                Toast.makeText(applicationContext,"${names[position]}", Toast.LENGTH_LONG).show()
-            }
-        }
+        listView.onItemClickListener =
+            AdapterView.OnItemClickListener { _, _, position, _ ->
+                Toast.makeText(applicationContext, names[position], Toast.LENGTH_LONG).show() }
     }
 }
