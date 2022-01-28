c14c98db46f13a34c353905444ad4e8c83898a0a
Muhammad Febrian Hasibuan <83405871+rian0502@users.noreply.github.com>
2022-01-28 19:09:56 +0700

    ah ah ah

--- app/src/main/java/com/belajar/kotlinandroid_1/MainActivity.kt
+++ app/src/main/java/com/belajar/kotlinandroid_1/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.belajar.kotlinandroid_1
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,37 +10,14 @@ import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("SetTextI18n")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val button_submit = findViewById<Button>(R.id.btnSubmit)
-        val button_clear = findViewById<Button>(R.id.btnClear)
-        val tfnama = findViewById<EditText>(R.id.tfNama)
-        val tfwa = findViewById<EditText>(R.id.tfWa)
-        val tfemail = findViewById<EditText>(R.id.tfEmail)
-        @SuppressLint("WrongViewCast")
-        val hasilview = findViewById<TextView>(R.id.tvhasil)
-
-        button_submit.setOnClickListener{
-            if (tfnama.text.isEmpty() || tfemail.text.isEmpty() || tfwa.text.isEmpty()){
-                Toast.makeText(this@MainActivity,"Isi Form dengan lengkap",Toast.LENGTH_LONG).show()
-            }else{
-                hasilview.setText("Nama : ${tfnama.text}\nWhatsApp : ${tfwa.text}\nEmail : ${tfemail.text}")
-                button_clear.setOnClickListener{
-                    tfnama.setText("")
-                    tfemail.setText("")
-                    tfwa.setText("")
-                    hasilview.setText("")
-                }
-            }
-        }
-        button_clear.setOnClickListener{
-            tfnama.setText("")
-            tfemail.setText("")
-            tfwa.setText("")
-            hasilview.setText("")
+        val toPageTwo = findViewById<Button>(R.id.toPageTwo);
+        toPageTwo.setOnClickListener{
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)
         }
     }
 
--- app/src/main/java/com/belajar/kotlinandroid_1/SecondActivity.kt
+++ app/src/main/java/com/belajar/kotlinandroid_1/SecondActivity.kt
@@ -1,11 +1,25 @@
 package com.belajar.kotlinandroid_1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+        val actionBar = supportActionBar
+        actionBar!!.title = "Halaman Ke-2"
+        actionBar.setDisplayHomeAsUpEnabled(true)
+
+        val listView = findViewById<ListView>(R.id.listView)
+        val names = arrayOf("android", "php", "c++", "java")
+        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter(
+            this, android.R.layout.simple_list_item_1, names
+        )
+        listView.adapter = arrayAdapter
     }
 }
